02f7fcecc5d9e87f298a6e8b7b99c39a344b738f
CodeDotJS <rushforlinux@gmail.com>
2016-09-18 18:45:19 +0530

    meta tweaks

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ test.cb('noQuery', t => {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout === `\n › Please provide a query\n\n For help       :   curt -h, --help\n For examples   :   curt -e, --examples\n\t\n`);
+		t.true(stdout === `\n › Please provide a query\n\n For help       :   currt -h, --help\n For examples   :   currt -e, --examples\n\t\n`);
 		t.end();
 	});
 });
@@ -27,7 +27,7 @@ test.cb('help', t => {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout === `\n Usage   : curt [query]\n\n query   : place, state, country, question\n\n Command :\n -e, --example       Display examples\n -h, --help          Display help\n -v, --version       Display version\n\t\n`);
+		t.true(stdout === `\n Usage   : currt [query]\n\n query   : place, state, country, question\n\n Command :\n -e, --example       Display examples\n -h, --help          Display help\n -v, --version       Display version\n\t\n`);
 		t.end();
 	});
 });
@@ -37,7 +37,7 @@ test.cb('examples', t => {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
-		t.true(stdout === `\n Examples : curt <query>\n\n where the format of query could be...\n\n ›  India\n › \"What is the time in New York\"\n › \"Time in Belgrade\"\n › \"What is the time in India when it is 7pm in New York USA\"\n › \"Whats the time in Chicago when its 8pm Eastern Time\"\n › \"If its 4pm in London what is the time in Boston\"\n › \"What is the time in Perl when it is 6pm on the 2nd of May in India\"\n › \"What time is it in Paris when it is 5-April-2015 in Sydney at 2am\"\n › \"When its midday on the 9th of September Pacific Time, whats the time in Miami\"\n › \"When does daylight saving time begin in Auckland\"\n › \"DST time in London\"\n › \"When does daylight saving time begin in New York in 2016\"\n\n paste any of the above mentioned query to check the result.\n\n NOTE : Use \" \" when your query has multiple words.\n \n`);
+		t.true(stdout === `\n Examples : currt <query>\n\n where the format of query could be...\n\n ›  India\n › \"What is the time in New York\"\n › \"Time in Belgrade\"\n › \"What is the time in India when it is 7pm in New York USA\"\n › \"Whats the time in Chicago when its 8pm Eastern Time\"\n › \"If its 4pm in London what is the time in Boston\"\n › \"What is the time in Perl when it is 6pm on the 2nd of May in India\"\n › \"What time is it in Paris when it is 5-April-2015 in Sydney at 2am\"\n › \"When its midday on the 9th of September Pacific Time, whats the time in Miami\"\n › \"When does daylight saving time begin in Auckland\"\n › \"DST time in London\"\n › \"When does daylight saving time begin in New York in 2016\"\n\n paste any of the above mentioned query to check the result.\n\n NOTE : Use \" \" when your query has multiple words.\n \n`);
 		t.end();
 	});
 });
